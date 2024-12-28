8262298f0e5b5548a380221d63708ded69db0193
Rayan <Rayan@gmail.com>
2024-12-28 17:20:08 +0330

    Add project files.

---
+++ MyEshop/Data/MyEshopContext.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyEshop.Models;
+
+namespace MyEshop.Data
+{
+    public class MyEshopContext : DbContext
+    {
+        public MyEshopContext(DbContextOptions<MyEshopContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryToProduct> CategoryToProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryToProduct>()
+                .HasKey(t => new { t.ProductId, t.CategoryId });
+
+            //modelBuilder.Entity<Product>(
+            //    p =>
+            //    {
+            //        p.HasKey(w => w.Id);
+            //        p.OwnsOne<Item>(w => w.Item);
+            //        p.HasOne<Item>(w => w.Item)
+            //            .WithOne(w => w.Product)
+            //            .HasForeignKey<Item>(w => w.Id);
+            //    }
+            //);
+
+            modelBuilder.Entity<Item>(i =>
+            {
+                i.Property(w => w.Price).HasColumnType("Money");
+                i.HasKey(w => w.Id);
+            });
+
+            #region Seed Data Category
+
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                Id = 1,
+                Name = "موبایل",
+                Description = "موبایل"
+            },
+                new Category()
+                {
+                    Id = 2,
+                    Name = "لوازم خانگی",
+                    Description = "لوازم خانگی"
+                },
+                new Category()
+                {
+                    Id = 3,
+                    Name = "لوازم تحریر",
+                    Description = "ساعت مچی"
+                }
+            );
+
+            modelBuilder.Entity<Item>().HasData(
+                new Item()
+                {
+                    Id = 1,
+                    Price = 854.0M,
+                    QuantityInStock = 5
+                },
+            new Item()
+            {
+                Id = 2,
+                Price = 3302.0M,
+                QuantityInStock = 8
+            }
+            //new Item()
+            //{
+            //    Id = 3,
+            //    Price = 2500,
+            //    QuantityInStock = 3
+            //}
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
+                    Id = 1,
+                    ItemId = 1,
+                    Name = "سامسونگ گلکسی S24",
+                    Description = "256 gb - 5g - Ram 12"
+                },
+                new Product()
+                {
+                    Id = 2,
+                    ItemId = 2,
+                    Name = "خودکار کیان",
+                    Description = "نوک 7 دهم"
+                });
+
+            modelBuilder.Entity<CategoryToProduct>().HasData(
+                new CategoryToProduct() { CategoryId = 1, ProductId = 1 },
+                new CategoryToProduct() { CategoryId = 2, ProductId = 1 },
+                new CategoryToProduct() { CategoryId = 3, ProductId = 1 },
+                new CategoryToProduct() { CategoryId = 1, ProductId = 2 },
+                new CategoryToProduct() { CategoryId = 2, ProductId = 2 },
+                new CategoryToProduct() { CategoryId = 3, ProductId = 2 }
+            );
+            #endregion
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MyEshop/Migrations/20241228133744_newupdat.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyEshop.Migrations
+{
+    public partial class newupdat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UsersUserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UsersUserId",
+                table: "Orders");
+
+            migrationBuilder.DeleteData(
+                table: "CategoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "CategoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "UsersUserId",
+                table: "Orders");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "موبایل", "موبایل" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "لوازم خانگی", "لوازم خانگی" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "لوازم تحریر");
+
+            migrationBuilder.InsertData(
+                table: "CategoryToProducts",
+                columns: new[] { "ProductId", "CategoryId" },
+                values: new object[,]
+                {
+                    { 2, 4 },
+                    { 1, 4 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "256 gb - 5g - Ram 12", "سامسونگ گلکسی S24" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "نوک 7 دهم", "خودکار کیان" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsersUserId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "Asp.Net Core 3", "Asp.Net Core" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "گروه لباس ورزشی", "لباس ورزشی" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "ساعت مچی");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 4, "لوازم منزل", "لوازم منزل" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "آموزش Asp.Net Core 3 پروژه محور ASP.NET Core بر پایه‌ی NET Core.استوار است و نگارشی از NET.محسوب می شود که مستقل از سیستم عامل و بدون واسط برنامه نویسی ویندوز عمل می کند.ویندوز هنوز هم سیستم عاملی برتر به حساب می آید ولی برنامه های وب نه تنها روز به روز از کاربرد و اهمیت بیشتری برخوردار می‌شوند بلکه باید بر روی سکوهای دیگری مانند فضای ابری(Cloud) هم بتوانند میزبانی(Host) شوند، مایکروسافت با معرفی ASP.NET Core گستره کارکرد NET.را افزایش داده است.به این معنی که می‌توان برنامه‌های کاربردی ASP.NET Core را بر روی بازه‌ی گسترده ای از محیط‌های مختلف میزبانی کرد هم‌اکنون می‌توانید پروژه های وب را برای Linux یا macOS هم تولید کنید.", "آموزش Asp.Net Core 3 پروژه محور" });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "در سال های گذشته ، کمپانی مایکروسافت با معرفی تکنولوژی های جدید و حرفه ای در زمینه های مختلف ، عرصه را برای سایر کمپانی ها تنگ تر کرده است. اخیرا این غول فناوری با معرفی فریم ورک های ASP.NET Core و همینطور Blazor قدرت خود در زمینه ی وب را به اثبات رسانده است.", "آموزش Blazor از مقدماتی تا پیشرفته" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ItemId", "Name" },
+                values: new object[] { 3, "آموزش اپلیکیشن های وب پیش رونده ( PWA ) آموزش PWA از مقدماتی تا پیشرفته وب اپلیکیشن‌های پیش رونده(PWA) نسل جدید اپلیکیشن‌های تحت وب هستند که می‌توانند آینده‌ی اپلیکیشن‌های موبایل را متحول کنند.در این دوره به طور جامع به بررسی آن‌ها خواهیم پرداخت. مزایا و فاکتور هایی که یک وب اپلیکیشن دارا می باشد به صورت زیر می باشد : ریسپانسیو :  رکن اصلی سایت برای PWA ریسپانسیو بودن اپلیکیشن هستش که برای صفحه نمایش کاربران مختلف موبایل و تبلت خود را وفق دهند.", 3, "آموزش اپلیکیشن های وب پیش رونده ( PWA )" });
+
+            migrationBuilder.InsertData(
+                table: "CategoryToProducts",
+                columns: new[] { "ProductId", "CategoryId" },
+                values: new object[,]
+                {
+                    { 3, 1 },
+                    { 3, 2 },
+                    { 3, 3 },
+                    { 3, 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UsersUserId",
+                table: "Orders",
+                column: "UsersUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UsersUserId",
+                table: "Orders",
+                column: "UsersUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
